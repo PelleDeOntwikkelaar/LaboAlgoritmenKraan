dddc29a1c96e30e0a72049c4ec5f650cd5f85000
PelleReyniers <pelle.reyniers@student.kuleuven.be>
2018-10-19 11:43:25 +0200

    klasse slots aangepast

--- src/be/kul/gantry/domain/Slots.java
+++ src/be/kul/gantry/domain/Slots.java
@@ -4,11 +4,47 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Slots {
-    private ArrayList<List> slotArrayYDimension;
+    private ArrayList<ArrayList> slotArrayYDimension;
+    private List<Item> items;
+    private int yDimension;
+    private int xDimenseion;
+    private boolean geschrankt;
 
-    public Slots(int yDimension) {
 
+    public Slots(List<Item> items,int yDimension,int xDimenseion,boolean geschrankt) {
+        this.items=items;
+        this.yDimension=yDimension;
+        this.xDimenseion=xDimenseion;
+        this.geschrankt=geschrankt;
         this.slotArrayYDimension = new ArrayList<>();
-        for()
+
+        for(int i=0;i<yDimension;i++){
+            slotArrayYDimension.add(new ArrayList<Slot>());
+        }
     }
+
+    public void addSlots(){
+
+    }
+
+    public boolean isSlotAvailable(Slot slot){
+        //todo: available slot methode schrijven.
+        return false;
+    }
+
+    public Slot findBestSlot(int zone){
+        //todo: implement method
+        return null;
+    }
+
+    public int findSlotByItem(){
+        //todo: implement method
+        return -1;
+    }
+
+
+
+
+
+
 }

f0b1e54b036895f75f05ab0f666fb9a31d565d2e
Boris Wauters <boris.wauters@telenet.be>
2018-11-09 10:14:35 +0100

    Keuze voor inputfile toegevoegd

--- src/be/kul/gantry/solution/Main.java
+++ src/be/kul/gantry/solution/Main.java
@@ -4,6 +4,7 @@ import be.kul.gantry.Extra.CSVFileWriter;
 import be.kul.gantry.domain.Problem;
 
 import java.io.File;
+import java.util.Scanner;
 
 /**
  * @author Pelle Reyniers & Boris Wauters
@@ -12,7 +13,24 @@ public class Main {
 
     public static void main(String[] args) {
 
-        File inputFile = new File("1_10_100_4_FALSE_65_50_50.json");
+        Scanner sc = new Scanner(System.in);
+        int choice = 0;
+
+        System.out.println("Choose your input file:");
+        System.out.println("    1. One gantry, stacked rows");
+        System.out.println("    2. One gantry, shifted rows");
+
+        choice = sc.nextInt();
+
+        File inputFile;
+        File oneGantryFalseFile = new File("1_10_100_4_FALSE_65_50_50.json");
+        File oneGantryTrueFile = new File("1_10_100_4_TRUE_65_50_50.json");
+        // File twoGantryTrueFile = new File("1_10_100_4_FALSE_65_50_50.json");
+
+        if (choice == 1) inputFile = oneGantryFalseFile;
+        else if (choice == 2) inputFile = oneGantryTrueFile;
+        else return;
+
         Problem problem;
         Solution solution;
         CSVFileWriter csvFileWriter = new CSVFileWriter();
@@ -27,6 +45,7 @@ public class Main {
 
         /*dat commando:
         * java -jar validator-v7.jar -debug 1_10_100_4_FALSE_65_50_50.json output.csv
+        * java -jar validator-v7.jar -debug 1_10_100_4_TRUE_65_50_50.json output.csv
         * */
 
 
